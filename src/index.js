dc07695ff7a4d0d10b23753eb3cfc95364f32576
MaksimTsurko <36657655+MaksimTsurko@users.noreply.github.com>
2018-02-28 11:50:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveEquation(equation) {
+   var str = equation.split(" ");
+   var a = str[0],b = str[3]+str[4],c =str[7]+str[8], result =[];
+   var D = Math.round((b*b) - (4*a*c));
+  var x1=Math.round((-b)+Math.sqrt(D))/(2*a), x2 = Math.round((-b)-Math.sqrt(D))/(2*a);
+  x2>x1? result = [x1,x2]: result = [x2,x1];
+  return result;
   // your implementation
 }
